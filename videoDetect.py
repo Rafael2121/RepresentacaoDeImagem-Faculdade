8088e3658a8dacdfbd927b659372fb0421762593
Milanez <rafaeldinoster@gmail.com>
2019-10-05 12:37:25 -0300

    Deleted test video from git

--- videoDetect.py
+++ videoDetect.py
@@ -1,14 +1,11 @@
 # -*- coding: UTF-8 -*-
-import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 fig = plt.figure()
 plt.rcParams['figure.figsize'] = (224, 224)
 
 face_cascade = cv2.CascadeClassifier('modelo/haarcascade_frontalface_default.xml')
-#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-# Create a VideoCapture object
 cap = cv2.VideoCapture("videos/video.mp4")
 
 i = 0
@@ -22,7 +19,6 @@ while (cap.isOpened()):
         j += 1
         plt.savefig("Hist/frame{}.png".format(j))
         
-        #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         faces = face_cascade.detectMultiScale(
             gray,
             scaleFactor=1.1,
@@ -37,8 +33,7 @@ while (cap.isOpened()):
             gray1 = cv2.cvtColor(roi_color, cv2.COLOR_BGR2GRAY)
             hist_full1 = cv2.calcHist([gray1],[0],None,[256],[0,256])
             plt.hist(hist_full1)            
-            plt.savefig("HistFace/framaFace{}.png".format(i))
-            
+            plt.savefig("HistFace/frame{}.png".format(i))  
     else: 
         break
 
